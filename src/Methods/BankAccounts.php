79f7172b3a631e5e310b6ce7af001a80f222e58e
Grisha Sychev <100209515+grisha-sychev@users.noreply.github.com>
2024-04-04 00:17:30 +0300

    Add methods Bank Accounts

--- src/Methods/BankAccounts.php
+++ src/Methods/BankAccounts.php
@@ -3,10 +3,36 @@
 namespace JumpFinance\Methods;
 
 use JumpFinance\Connect\Authorization;
+
 class BankAccounts extends Authorization
 {
     function __construct()
     {
         $this->checkClientKey();
+    }
+
+    public function getAll(string $view = 'simple')
+    {
+        return $this->get(
+            "banks_accounts",
+            [
+                'view' => $view
+            ]
+        );
+    }
+
+    public function getBankAccount(string $bank_account_id)
+    {
+        return $this->get("banks_accounts/" . $bank_account_id);
+    }
+
+    public function updateBalancesAllBankAccounts()
+    {
+        return $this->post("banks_accounts/sync_balances");
+    }
+
+    public function getInfoRequestUpdateBankAccounts(string $sync_balances_status_id)
+    {
+        return $this->get("banks_accounts/sync_balances_status/" . $sync_balances_status_id);
     }
 }
